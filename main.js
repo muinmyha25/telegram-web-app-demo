d80cf575ac2525a5e33bb2c7b58b2aa85f191412
muinmyha25 <romajvl@gmail.com>
2025-06-05 21:51:55 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
+document.addEventListener("DOMContentLoaded", async () => {
+    const yearPicker = document.getElementById("yearPicker");
+    const totalEarnings = document.getElementById("totalEarnings");
+    const totalExpenses = document.getElementById("totalExpenses");
 
+    const response = await fetch('/api/stats');
+    const data = await response.json();
+
+    totalEarnings.textContent = `${data.totalEarnings} ₽`;
+    totalExpenses.textContent = `${data.totalExpenses} ₽`;
+
+    const currentYear = new Date().getFullYear();
+    for (let i = currentYear - 5; i <= currentYear + 5; i++) {
+        const option = document.createElement("option");
+        option.value = i;
+        option.textContent = i;
+        yearPicker.appendChild(option);
+    }
+
+    yearPicker.value = currentYear;
+});
